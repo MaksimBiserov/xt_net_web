2e894d4c64f023d9f7781284da013117e5b83ad2
MaksimBiserov <maxtyge@mail.ru>
2020-07-08 01:53:40 +0400

    Added subtask 3_3_1_SUPER_ARRAY

---
+++ Task_3_3_Pizza_Time/Task_3_3_1_SUPER ARRAY/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+
+namespace Task_3_3_1_SUPER_ARRAY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UseExtensionMethods();
+        }
+        private static void UseExtensionMethods()
+        {
+            double[] array = new double[] { 1, 2, 2, 3, 3, 3, 4, 5 };
+            WriteLine("Исходный массив:");
+            foreach (var item in array)
+            {
+                Write(item + "   ");
+            }
+            WriteLine();
+            double[] array2 = array.ChangeArrayElements(ArrayExtension.MultiplyByTwo);
+            WriteLine("Массив после обработки MultiplyByTwo:");
+            foreach (var item in array2)
+            {
+                Write(item + "   ");
+            }
+            WriteLine();
+            double[] array3 = array.ChangeArrayElements(ArrayExtension.SquareIt);
+            WriteLine("Массив после обработки SquareIt:");
+            foreach (var item in array3)
+            {
+                Write(item + "   ");
+            }
+            WriteLine();
+            double sum = array.SearchArrayValues(ArrayExtension.SumArrayElements);
+            WriteLine("Сумма элементов исходного массива равна " + sum);
+            double average = array.SearchArrayValues(ArrayExtension.AverageArrayElements);
+            WriteLine("Среднее значение элементов исходного массива равно " + average);
+            double common = array.SearchArrayValues(ArrayExtension.CommonArrayElement);
+            WriteLine("Наиболее часто повторяющееся значение элементов исходного массива равно " + common);
+            ReadKey();
+        }
+    }
+}
